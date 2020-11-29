6493c7fdfe1bd874a21d35d48886c89b611bc166
gillianbc <gszemeti@gmail.com>
2020-11-29 17:26:55 +0000

    Section 1 - Lec 8 - First React working (verbose)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,19 @@
-console.log('Hello from app.js')
+"use strict";
 
 //JSX  Javascript XML - extends the JS language
 
-var template = <p>This is JSX from app.js</p>;
+// var template = <p>This is JSX from app.js</p>;
+var appRoot = document.getElementById('app');
+
+
+
+
+var template = /*#__PURE__*/ React.createElement(
+  "p",
+  {
+    id: "abc"
+  },
+  "Hello from React"
+);
+
+ReactDOM.render(template, appRoot);
