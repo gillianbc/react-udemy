3dab2b96e7766d97ed8eae0ea32ff046dccbcc1d
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2021-01-05 21:14:13 +0000

    Refactored as implicit return arrow functions

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
 // Stateless functional component
 import React from "react";
 
-const Header = (props) => {
-    return (
-        /*Header has been called with 2 args, these become props*/
-        <div>
-            <h1>{props.title}</h1>
-            {props.subtitle && <h2>{props.subtitle}</h2>}
-        </div>
-    )
-}
+const Header = (props) => (
+    /*Header has been called with 2 args, these become props*/
+    <div>
+        <h1>{props.title}</h1>
+        {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+)
 
 Header.defaultProps = {
     title: 'My Application Title'
--- src/components/Options.js
+++ src/components/Options.js
@@ -2,29 +2,27 @@
 import Option from "./Option";
 import React from "react";
 
-const Options = (props) => {
-    return (
-        <div>
-            {props.options.length === 0 && <p>Please add an option to get started</p>}
-            {
-                /*To do a simple paragraph, we'd do this
-                this.props.options.map((option) =>  <p key={option}>{option}</p>)
-                */
-                /*We still need to provide a key as it's an array*/
-                props.options.map((option) =>
-                    <Option
-                        key={option}
-                        optionText={option}
-                        handleDeleteOption={props.handleDeleteOption}
-                    />)
-            }
-            <button
-                onClick={props.handleRemove}
-                disabled={props.options.length < 2}>
-                Remove all options?
-            </button>
-        </div>
-    )
-}
+const Options = (props) => (
+    <div>
+        {props.options.length === 0 && <p>Please add an option to get started</p>}
+        {
+            /*To do a simple paragraph, we'd do this
+            this.props.options.map((option) =>  <p key={option}>{option}</p>)
+            */
+            /*We still need to provide a key as it's an array*/
+            props.options.map((option) =>
+                <Option
+                    key={option}
+                    optionText={option}
+                    handleDeleteOption={props.handleDeleteOption}
+                />)
+        }
+        <button
+            onClick={props.handleRemove}
+            disabled={props.options.length < 2}>
+            Remove all options?
+        </button>
+    </div>
+)
 
 export default Options
